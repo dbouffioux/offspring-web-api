b1adbe570925b4727e9db49036d433c6a64af276
Maxime Quoilin <max.quoilin@gmail.com>
2019-07-01 11:22:43 +0200

    modif

--- src/be/technifutur/offspring/repository/DataRepository.java
+++ src/be/technifutur/offspring/repository/DataRepository.java
@@ -1,5 +1,7 @@
 package be.technifutur.offspring.repository;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,6 +9,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import be.technifutur.offspring.beans.Activity;
 
@@ -14,37 +17,34 @@ public class DataRepository {
 	private String url;
 	private String user;
 	private String password;
-	
+
 	public DataRepository(String url, String user, String password) {
 		super();
 		this.url = url;
 		this.user = user;
 		this.password = password;
 	}
-	
+
 	protected Connection createConnection() throws SQLException {
 		return DriverManager.getConnection(url, user, password);
 	}
-	
+
 	public List<Activity> findAllActivity() {
 		
 		List<Activity> list = new ArrayList<>();
-		String sql = "SELECT name as activity_name "
-				+ "FROM activity";
-		try (
-			Connection connection = createConnection();
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(sql)
-		) {
+		String sql = "SELECT name as activity_name " + "FROM activity";
+		try (Connection connection = createConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(sql)) {
 			while (resultSet.next()) {
 				Activity activity = createActivity(resultSet);
 				list.add(activity);
 			}
-		} catch(SQLException sqle) {
+		} catch (SQLException sqle) {
 			throw new RuntimeException(sqle);
 		}
 		return list;
-		
+
 	}
 
 	private Activity createActivity(ResultSet rs) throws SQLException {
@@ -52,4 +52,7 @@ public class DataRepository {
 		Activity activity = new Activity(name);
 		return activity;
 	}
+
+	
+
 }
--- src/be/technifutur/offspring/servlet/OffspringJsonServlet.java
+++ src/be/technifutur/offspring/servlet/OffspringJsonServlet.java
@@ -64,6 +64,6 @@ public class OffspringJsonServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
 	}
 }
