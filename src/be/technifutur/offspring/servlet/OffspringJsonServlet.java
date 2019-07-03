249892a3b3e3201a0524818da387caf6ce967673
Damien Bouffioux <damien.bouffioux@gmail.com>
2019-07-03 14:47:18 +0200

    reworked headers set

--- src/be/technifutur/offspring/servlet/OffspringJsonServlet.java
+++ src/be/technifutur/offspring/servlet/OffspringJsonServlet.java
@@ -128,12 +128,7 @@ public class OffspringJsonServlet extends HttpServlet {
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		response.getWriter().write(json);
-	}
-	
-	@Override
-	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		super.doOptions(request, response);
-		setHeaders(response);
+		this.setHeaders(response);
 	}
 	
 	private void setHeaders( HttpServletResponse response ) {
